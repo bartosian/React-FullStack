4786c81215296fadb3d09721bf0c8cb7c9d8e531
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-28 15:34:59 -0400

    Finished signup component

--- profile-app/public-react/src/App.js
+++ profile-app/public-react/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import  HomePage  from './components/HomePage/HomePage';
+import Signup from './components/auth/Signup/Signup';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Switch>
-          <Route path="/" component={ HomePage } />
+          <Route exact path="/" component={ HomePage } />
+          <Route exact path="/signup" component={ Signup }/>
       </Switch>
     );
   }
--- profile-app/public-react/src/components/auth/Signup/Signup.js
+++ profile-app/public-react/src/components/auth/Signup/Signup.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AuthService from '../auth-service';
 import { Link } from 'react-router-dom';
+import AppButton from '../../../components/UI/AppButton/AppButton';
 
 class Signup extends Component {
     constructor(props){
@@ -41,7 +42,36 @@ class Signup extends Component {
 
     render(){
         return(
-            // more code will be added here
+            <div className="home-wrapper">
+                <form onSubmit={this.handleFormSubmit}>
+                    <label>Username:</label>
+                    <input type="text" name="username" value={this.state.username} onChange={ e => this.handleChange(e)}/>
+
+                    <label>Password</label>
+                    <input name="password" value={this.state.password} onChange={ e => this.handleChange(e)} />
+
+                    <label>Campus</label>
+                    <select type="text" name="campus" value={this.state.campus} onChange={ e => this.handleChange(e)}>
+                        <option value="Madrid">Madrid</option>
+                        <option value="Barcelona">Barcelona</option>
+                        <option value="Miami">Miami</option>
+                        <option value="Paris">Paris</option>
+                        <option value="Berlin">Berlin</option>
+                        <option value="Amsterdam">Amsterdam</option>
+                        <option value="Mexico">Mexico</option>
+                        <option value="Sao Paolo">Sao Paolo</option>
+                    </select>
+
+                    <label>Course</label>
+                    <select type="text" name="course" value={this.state.campus} onChange={ e => this.handleChange(e)}>
+                        <option value="WebDev">WebDev</option>
+                        <option value="UX/UI">UX/UI</option>
+                        <option value="Data Science">Data Science</option>
+                    </select>
+
+                    <AppButton bg="white" type="submit">Create the account</AppButton>
+                </form>
+            </div>
         )
     }
 }
